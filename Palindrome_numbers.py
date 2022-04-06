70c047388049e27c4ee819be37a3e62400761d46
senthil625001 <100005602+senthil625001@users.noreply.github.com>
2022-04-06 09:46:21 -0700

    Create Palindrome_numbers.py

---
+++ Palindrome_numbers.py
@@ -0,0 +1,17 @@
+# palindrome program in python using while loop
+
+# take inputs
+num = 11
+# calculate reverse of number
+reverse = 0
+number = num
+while(num != 0):
+   remainder = num % 10
+   reverse = reverse * 10 + remainder
+   num = int(num / 10)
+
+# compare reverse to original number
+if(number == reverse):
+   print(number,'is a Palindrome')
+else:
+   print(number,'is not a Palindrome')
